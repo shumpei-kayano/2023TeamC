d845222cbc8cf13d791b5770576b8a1062e1837f
shumpei-kayano <>
2023-10-26 14:58:01 +0900

    diary

---
+++ src/diary/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from user import views
+
+app_name = 'diary'
+urlpatterns = [
+    path('positive_conversion', views.CustomUserCreate, name='positive_conversion'),
+
+]
