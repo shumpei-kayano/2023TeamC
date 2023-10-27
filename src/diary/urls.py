fb6b131cde1d54a55c5bfb153f665383d5c301e8
ooi2272403 <iso1019r1@icloud.com>
2023-10-27 14:47:43 +0900

    DiaryURLを変更しました。

--- src/diary/urls.py
+++ src/diary/urls.py
@@ -1,15 +1,38 @@
-from django.urls import path
-from diary import views
+# from django.urls import path
+# from diary import views
+# from user import views
 
-app_name = 'diary'
-urlpatterns = [
-    path('account_delete_success', views.account_delete_success, name='account_delete_success'),
-    path('account_delete', views.account_delete,name='account_delete'),
-    path('base', views.base,name='base'),
-    path('calendar_month', views.calendar_month,name='calendar_month'),
-    path('calender_week', views.calendar_month,name='calender_week'),
-    path('create_diary_confirmation', views.calendar_month,name='create_diary_confirmation'),
-    path('create_diary', views.calendar_month,name='create_diary'),
-    path('diary_delete', views.calendar_month,name='diary_delete'),
-    path('diary_graph', views.calendar_month,name='diary_graph'),
-]
+# app_name = 'diary'
+# urlpatterns = [
+#     path('account_delete_success', views.account_delete_success, name='account_delete_success'),
+#     path('account_delete', views.account_delete,name='account_delete'),
+#     path('base', views.base,name='base'),
+#     path('calendar_month', views.calendar_month,name='calendar_month'),
+#     path('calender_week', views.calendar_month,name='calender_week'),
+#     path('create_diary_confirmation', views.calendar_month,name='create_diary_confirmation'),
+#     path('create_diary', views.create_diary,name='create_diary'),
+#     path('diary_delete', views.diary_delete,name='diary_delete'),
+#     path('diary_graph', views.diary_graph,name='diary_graph'),
+#     path('diary_home', views.diary_home,name='diary_home'),
+#     path('diary_update', views.diary_update,name='diary_update'),
+#     path('help_calender', views.help_calender,name='help_calender'),
+#     path('help_diary_edit', views.help_diary_edit,name='help_diary_edit'),
+#     path('help_diary', views.help_diary,name='help_diary'),
+#     path('help_graph', views.help_graph,name='help_graph'),
+#     path('help', views.help,name='help'),
+#     path('home_top', views.home_top,name='home_top'),
+#     path('loading', views.loading,name='loading'),
+#     path('logout_success', views.logout_success,name='logout_success'),
+#     path('logout', views.logout,name='logout'),
+#     path('member_information_edit_cancel', views.member_information_edit_cancel,name='member_information_edit_cancel'),
+#     path('member_information_edit_check', views.member_information_edit_check,name='member_information_edit_check'),
+#     path('member_information_edit_comp', views.member_information_edit_comp,name='member_information_edit_comp'),
+#     path('member_information_edit', views.member_information_edit,name='member_information_edit'),
+#     path('member_information', views.member_information,name='member_information'),
+#     path('month_graph', views.month_graph,name='month_graph'),
+#     path('positive_conversion', views.positive_conversion,name='positive_conversion'),
+#     path('setting', views.setting,name='setting'),
+#     path('today_counseling', views.today_counseling,name='today_counseling'),
+#     path('today_diary_detail_', views.today_diary_detail_,name='today_diary_detail_'),
+#     path('week_graph', views.week_graph,name='week_graph'),
+# ]
--- src/diary/views.py
+++ src/diary/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
-from .models import Diary, emotion
-from user.models import CustomUser
+# from django.shortcuts import render
+# from django.views.generic.edit import FormView
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# # from .models import Diary, emotion
+# # from user.models import CustomUser
 
-<<<<<<< HEAD
-def account_delete_success(request):
-    return render(request, 'diary/account_delete_success.html')
-=======
-#def positive_conversion(request):
- #   return render(request, 'diaty/positive_conversion.html')
->>>>>>> 8feef947f13e45b0015982f319a537009a98d6d4
+# def account_delete_success(request):
+#     return render(request, 'diary/account_delete_success.html')
+# def account_delete(request):
+#     return render(request, 'diary/account_delete.html')
+# def base(request):
+#     return render(request, 'diary/base.html')
+# def calendar_month(request):
+#     return render(request, 'diary/calendar_month.html')
+# def calender_week(request):
+#     return render(request, 'diary/calender_week.html')
+# def create_diary_confirmation(request):
+#     return render(request, 'diary/create_diary_confirmation.html')
+# def create_diary(request):
+#     return render(request, 'diary/create_diary.html')
+# def diary_delete(request):
+#     return render(request, 'diary/diary_delete.html')
+# def diary_graph(request):
+#     return render(request, 'diary/diary_graph.html')
+# def diary_home(request):
+#     return render(request, 'diary/diary_home.html')
+# def diary_update(request):
+#     return render(request, 'diary/diary_update.html')
+# def help_calender(request):
+#     return render(request, 'diary/help_calender.html')
+# def help_diary_edit(request):
+#     return render(request, 'diary/help_diary_edit.html')
+# def help_diary(request):
+#     return render(request, 'diary/help_diary.html')
+# def help_graph(request):
+#     return render(request, 'diary/help_graph.html')
+# def help(request):
+#     return render(request, 'diary/help.html')
+# def home_top(request):
+#     return render(request, 'diary/home_top.html')
+# def loading(request):
+#     return render(request, 'diary/loading.html')
+# def logout_success(request):
+#     return render(request, 'diary/logout_success.html')
