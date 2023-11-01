f7c275c04e5a4e9f2e88f0cc46eb617d7e184187
ooi2272403 <iso1019r1@icloud.com>
2023-11-01 14:01:15 +0900

    チュートリアル追加

--- src/user/urls.py
+++ src/user/urls.py
@@ -4,4 +4,5 @@ from user import views
 app_name = 'user'
 urlpatterns = [
     path('account_top/', views.account_top,name='account_top'),
-]    
+    path('tutorial1/', views.tutorial1,name='tutorial1'),
+    ]    
