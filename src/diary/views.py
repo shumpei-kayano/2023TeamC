0e3b6f62a4c50814c5caa680bd65baa5e0385966
shumpei-kayano <>
2023-11-29 10:13:12 +0900

    削除

--- src/diary/views.py
+++ src/diary/views.py
@@ -439,6 +439,8 @@ def month_graph(request,selected_date=None):
         month_ai=Month_AI.objects.get(user = request.user,created_date__year = year,created_date__month = month)
         ai_comment = month_ai.ai_comment
     else:
+      month_ai=Month_AI.objects.get(user = request.user,created_date__year = year,created_date__month = month)
+      month_ai.delete
       ai_comment = '15日以上日記をかいてくにゃさい'
     data = chart_data(emotion)
     chart_data_json = JsonResponse(data, safe=False).content.decode('utf-8')
@@ -554,6 +556,8 @@ def week_graph(request,selected_date=None):
         week_ai=Week_AI.objects.get(user = request.user,created_date__range=[start_date,one_week_str])
         ai_comment = week_ai.ai_comment
     else:
+      week_ai=Week_AI.objects.get(user = request.user,created_date__range=[start_date,one_week_str])
+      week_ai.delete
       ai_comment = '4日以上日記をかいてくにゃさい'
     data = chart_data(emotions)
     chart_data_json = JsonResponse(data, safe=False).content.decode('utf-8')
