2ae579ac2e5189004407b54910bf771203a67728
shumpei-kayano <>
2024-01-11 11:50:00 +0900

    syuusei

--- src/user/urls.py
+++ src/user/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('account_top/', views.account_top,name='account_top'),
     path('tutorial1/', views.tutorial1,name='tutorial1'),
     path('logout', views.logout,name='logout'),
-     path('accounts/signup/', CustomSignupView.as_view(), name='account_signup'),
+    path('accounts/signup/', CustomSignupView.as_view(), name='account_signup'),
     ]    
