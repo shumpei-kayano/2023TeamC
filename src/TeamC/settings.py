60f4b489a4ff65764f216dde0272ff784362ab6d
shumpei-kayano <>
2024-02-09 10:55:54 +0900

    ip設定

--- src/TeamC/settings.py
+++ src/TeamC/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-^kcmqusi%2#6(yl+c3f(7d36c10ak$)2f9)-)qa0nu*@^41pm@
 DEBUG = False
 
 # 本番環境だと変える
-ALLOWED_HOSTS = ['44.223.51.18', 'localhost']
+ALLOWED_HOSTS = ['34.233.79.231,52.201.75.237']
 
 
 
